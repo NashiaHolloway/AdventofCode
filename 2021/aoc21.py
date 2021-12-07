d7698e0d4a9041e5bacb35d84104442a1f167acd
Nashia Holloway <nashiaholloway@protonmail.com>
2021-12-06 22:27:44 -0500

    day1 complete

--- 2021/aoc21.py
+++ 2021/aoc21.py
@@ -1,9 +1,23 @@
 
-def day1():
-    
+def day1(day1input):
+    increased = 1
+    with open(day1input, "r") as file:
+            lines = file.readlines()
+            for i in range(0, len(lines)):
+                if i+1 < len(lines):
+                    if lines[i] < lines[i+1]:
+                        print(lines[i+1], " (INCREASED)")
+                        i += 1
+                        increased += 1
+                    else:
+                        print(lines[i+1], " (DECREASED)")
+                else:
+                    print("Number of increases: ", increased)
+    file.close()
 
 def main():
-    day1()
+    day1input = "day1input.txt"
+    day1(day1input)
 
 
 if __name__ == '__main__':
