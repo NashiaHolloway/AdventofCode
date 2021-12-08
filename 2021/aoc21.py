ed820438d527b054752dd1e2277d1a8f0592dc01
Nashia Holloway <nashiaholloway@protonmail.com>
2021-12-07 20:26:47 -0500

    aoc day2 complete

--- 2021/aoc21.py
+++ 2021/aoc21.py
@@ -1,8 +1,9 @@
 
-def day1(day1input):
+def SonarSweep(day1input):
     increased = 1
     with open(day1input, "r") as file:
             lines = file.readlines()
+            file.close()
             for i in range(0, len(lines)):
                 if i+1 < len(lines):
                     if lines[i] < lines[i+1]:
@@ -13,11 +14,39 @@ def day1(day1input):
                         print(lines[i+1], " (DECREASED)")
                 else:
                     print("Number of increases: ", increased)
-    file.close()
+    
+
+def Dive(day2input):
+    h = 0 # horizontal position
+    d = 0 # depth
+    with open(day2input, "r") as file:
+        lines = file.readlines()
+        file.close()
+        for line in lines:
+            instruction = line.split()
+            if instruction[0] == "forward":
+                h += int(instruction[1])
+                print("forward", h)
+            elif instruction[0] == "down":
+                d += int(instruction[1])
+                print("down", d)
+            else:
+                d -= int(instruction[1])
+                print("up", d)
+    print("Final Horizontal Position:", h)
+    print("Final depth:", d)
+    ans = h*d
+    print("Answer:", ans)
 
 def main():
-    day1input = "day1input.txt"
-    day1(day1input)
+    # day1input = "day1input.txt"
+    # SonarSweep(day1input)
+
+    day2input = "day2input.txt"
+    Dive(day2input)
+
+    day2_5input = "day2_5input.txt"
+    Dive2(day2_5input)
 
 
 if __name__ == '__main__':
